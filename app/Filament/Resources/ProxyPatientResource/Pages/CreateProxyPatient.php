b8ba48fd405f16f7445beb1524d3496c08aa1bb0
silasmas <silasjmas@gmail.com>
2026-03-09 14:31:34 +0100

    ok

--- app/Filament/Resources/ProxyPatientResource/Pages/CreateProxyPatient.php
+++ app/Filament/Resources/ProxyPatientResource/Pages/CreateProxyPatient.php
@@ -3,10 +3,26 @@
 namespace App\Filament\Resources\ProxyPatientResource\Pages;
 
 use App\Filament\Resources\ProxyPatientResource;
-use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Support\Facades\Auth;
 
 class CreateProxyPatient extends CreateRecord
 {
     protected static string $resource = ProxyPatientResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $data = parent::mutateFormDataBeforeCreate($data);
+
+        if (($data['relation'] ?? null) === 'self') {
+            $data['user_id'] = $data['user_id'] ?? Auth::id();
+        }
+
+        // created_by = user parent (user_id), obligatoire pour la colonne NOT NULL
+        $data['created_by'] = $data['user_id'] ?? Auth::id();
+        $data['updated_by'] = $data['updated_by'] ?? Auth::id() ?? $data['created_by'];
+        $data['status']     = $data['status'] ?? 1;
+
+        return $data;
+    }
 }
